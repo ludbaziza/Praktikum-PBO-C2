b32d3fa59e3e98f6558186a7371e514beb816f93
ludbaziza <100133942+ludbaziza@users.noreply.github.com>
2023-03-01 20:19:39 +0700

    Update MOperasiTitik.java

--- Praktikum 2/MOperasiTitik.java
+++ Praktikum 2/MOperasiTitik.java
@@ -1,9 +1,8 @@
 /*
-	Muhammad Abdul Aziz
-	24060121140150
-	LAB PBO C2
-	01/03/2023
-*/
+ * File      : MOperasiTitik.java	(01/03/23)	
+ * Penulis   : Muhammad Abdul Aziz
+ * Deskripsi : Kelas yang berisi main dari MOperasiTitik
+ */
 
 class MOperasiTitik{
 	public static void main(String[] args){
