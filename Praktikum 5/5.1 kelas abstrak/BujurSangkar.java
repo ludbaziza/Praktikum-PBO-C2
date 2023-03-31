8ca34f6496a1da14beb1260ae09609cc60f742bb
ludbaziza <100133942+ludbaziza@users.noreply.github.com>
2023-03-31 16:00:23 +0700

    Add files via upload

---
+++ Praktikum 5/5.1 kelas abstrak/BujurSangkar.java
@@ -0,0 +1,11 @@
+/**
+ * File : BujurSangkar.java 31/03/2023
+ * Penulis : Muhammad Abdul Aziz
+ * Deskripsi : kelas yang berisi implementasi dari kelas abstrak bangundatar
+ */
+
+public class BujurSangkar extends BangunDatar{
+    public double hitungLuas(double sisi) {
+        return sisi * sisi;
+    }
+}
