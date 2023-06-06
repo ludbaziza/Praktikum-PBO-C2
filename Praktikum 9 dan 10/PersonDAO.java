9613e481616cd7402a79dde3db3d89e39ecbf524
ludbaziza <100133942+ludbaziza@users.noreply.github.com>
2023-06-06 20:29:01 +0700

    Delete Praktikum 9 dan 10 directory

--- Praktikum 9 dan 10/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-package com.PersistentObject;
-
-/**
-File : PersonDAO.java
-Deskripsi : interface untuk person access object
-*/
-public interface PersonDAO{
-	public void savePerson(Person p) throws Exception;
-}
